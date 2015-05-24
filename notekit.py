9bdee6764132abb57d447e0c4d66c7e72aa08ab2
sq3 <mail@mschnitzius.com>
2015-05-24 11:06:20 +0200

    start creating notekit.py with argparse and including mdrender

--- notekit.py
+++ notekit.py
@@ -1,23 +1,52 @@
 #!/usr/bin/env python
 
+import argparse
+import codecs
+import markdown
 import mdrender
+import notekit_html
 import settings
 import sys
 import subprocess
 
-def scp(source, server, path = settings.remote_note_path):
-	return not subprocess.Popen(["scp", source, "%s:%s" % (server, path)]).wait()
+parser = argparse.ArgumentParser()
+parser.add_argument('--publish', '-pub', help='Publishe your note')
+parser.add_argument('--preview', '-pre', help='Shows your renderd note')
+args = parser.parse_args()
 
-def main(*args):
-	filename = "example-note"
-	server = settings.server
+def publish(args):
+    def scp(source, server, path = settings.remote_note_path):
+    	return not subprocess.Popen(["scp", source, "%s:%s" % (server, path)]).wait()
+    	filename = "example-note"
+    	server = settings.server
+    
+    	if scp(filename, server):
+    		print("File uploaded successfully.")
+    		return 0
+    	else:
+    		print("File upload failed.")
+    		return 1
 
-	if scp(filename, server):
-		print("File uploaded successfully.")
-		return 0
-	else:
-		print("File upload failed.")
-		return 1
+def preview(args):
+    md_note = "/tmp/tmp/test-note.md"
+    html_note = "/tmp/tmp/RENDERD_TEST_NOTE.html"
+    
+    input_file = codecs.open(
+            md_note,
+            mode="r",
+            encoding="utf-8"
+            )
+    
+    text = input_file.read()
+    
+    html = markdown.markdown(text)
+    
+    output_file = codecs.open(
+            html_note,
+            "w",
+            encoding="utf-8",
+            errors="xmlcharrefreplace"
+            )
+    
+    output_file.write(notekit_html.HEADER + html + notekit_html.FOOTER)
 
-
-if __name__ == "__main__": sys.exit(main(*sys.argv[1:]))
