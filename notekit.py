cf5470e655c0a7e0fc5fed5ac10747fd2b60523c
sq3 <mail@mschnitzius.com>
2015-05-24 11:46:27 +0200

    show help when running notekit without options. Also some formatting stuff. Option preview is now called show to use -s as shortcut.

--- notekit.py
+++ notekit.py
@@ -9,12 +9,8 @@ import settings
 import sys
 import subprocess
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--publish', '-pub', help='Publishe your note')
-parser.add_argument('--preview', '-pre', help='Shows your renderd note')
-args = parser.parse_args()
 
-def publish(args):
+def publish(parsed_args):
     def scp(source, server, path = settings.remote_note_path):
     	return not subprocess.Popen(["scp", source, "%s:%s" % (server, path)]).wait()
     	filename = "example-note"
@@ -27,26 +23,27 @@ def publish(args):
     		print("File upload failed.")
     		return 1
 
-def preview(args):
+def show(parsed_args):
     md_note = "/tmp/tmp/test-note.md"
     html_note = "/tmp/tmp/RENDERD_TEST_NOTE.html"
     
-    input_file = codecs.open(
-            md_note,
-            mode="r",
-            encoding="utf-8"
-            )
+    input_file = codecs.open(md_note, mode="r", encoding="utf-8")
     
     text = input_file.read()
     
     html = markdown.markdown(text)
     
-    output_file = codecs.open(
-            html_note,
-            "w",
-            encoding="utf-8",
-            errors="xmlcharrefreplace"
-            )
+    output_file = codecs.open(html_note, "w", encoding="utf-8", errors="xmlcharrefreplace")
     
     output_file.write(notekit_html.HEADER + html + notekit_html.FOOTER)
 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--publish', '-p', dest='action', action='store_const', const=publish, help='Publishe your note')
+parser.add_argument('--show', '-s', dest='action', action='store_const', const=show, help='Shows you a preview of your renderd note')
+
+parsed_args = parser.parse_args()
+if parsed_args.action is None:
+    parser.parse_args(['-h'])
+parsed_args.action(parsed_args)
