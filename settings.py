7a629666e9aa97652ebc44f3dadd936d35ace176
sq3 <mail@mschnitzius.com>
2015-05-25 10:03:20 +0200

    new settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 remote_note_path = "/var/www/notes/"
-base_url = "http://kasten.beer/notes/"
+base_url = "http://mschnitzius.com/notes/"
 preview = "/tmp/tmp/"
 server = "playground"
